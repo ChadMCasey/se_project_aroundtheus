7992c9b206c1238e1b98b6c1a4722805e9d07bed
ChadCasey <gbchadd@gmail.com>
2024-02-17 07:59:44 -0700

    conslidate js to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
+let modalCloseButton = document.querySelector(".modal__close-btn");
+let profileEditButton = document.querySelector(".profile__edit-button");
+let modal = document.querySelector(".modal");
+
+function exitModal(e) {
+  e.preventDefault();
+  modal.classList.remove("modal_opened");
+}
+
+function profileEditOpenModal(e) {
+  modal.classList.add("modal_opened");
+}
+
+modalCloseButton.addEventListener("click", (e) => {
+  exitModal(e);
+});
+profileEditButton.addEventListener("click", (e) => {
+  profileEditOpenModal(e);
+});
+
 const initialCards = [
   {
     name: "Yosemite Valley",
