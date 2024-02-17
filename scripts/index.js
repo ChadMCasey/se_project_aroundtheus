3b930fdf4ba02f838ed5c12d60456c969af5aea6
ChadCasey <gbchadd@gmail.com>
2024-02-17 13:00:34 -0700

    added profile edit functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,21 @@
-let modalCloseButton = document.querySelector(".modal__close-btn");
-let profileEditButton = document.querySelector(".profile__edit-button");
+// modal
 let modal = document.querySelector(".modal");
+let modalForm = modal.querySelector(".modal__form");
+let modalCloseButton = modal.querySelector(".modal__close-btn");
+let modalSaveButton = modalForm.querySelector(".modal__save-btn");
+let modalInputName = modalForm.querySelector(".modal__input_type_name");
+let modalInputDescription = modalForm.querySelector(
+  ".modal__input_type_description"
+);
 
-function exitModal(e) {
-  e.preventDefault();
-  modal.classList.remove("modal_opened");
-}
+// profile
+let profile = document.querySelector(".profile");
+let profileEditButton = profile.querySelector(".profile__edit-button");
+let profileName = profile.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__avocation");
 
-function profileEditOpenModal(e) {
-  modal.classList.add("modal_opened");
-}
-
-modalCloseButton.addEventListener("click", (e) => {
-  exitModal(e);
-});
-profileEditButton.addEventListener("click", (e) => {
-  profileEditOpenModal(e);
-});
+// cards list
+let cardsList = document.querySelector(".cards__list");
 
 const initialCards = [
   {
@@ -44,3 +43,37 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+function exitModal(e) {
+  e.preventDefault();
+  modal.classList.remove("modal_opened");
+}
+
+function openModal(e) {
+  modal.classList.add("modal_opened");
+  modalInputName.value = profileName.textContent;
+  modalInputDescription.value = profileDescription.textContent;
+}
+function submitModal(e) {
+  e.preventDefault();
+  profileName.textContent = modalInputName.value;
+  profileDescription.textContent = modalInputDescription.value;
+  modal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+  let cardTemplate = document.querySelector("#card-template").content;
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__img").src = data.link;
+  cardElement.querySelector(".card__img").alt = data.name;
+  cardElement.querySelector(".card__title").textContent = data.name;
+  return cardElement;
+}
+
+modalCloseButton.addEventListener("click", exitModal);
+profileEditButton.addEventListener("click", openModal);
+modal.addEventListener("submit", submitModal);
+
+initialCards.forEach((cardObj) => {
+  cardsList.append(getCardElement(cardObj));
+});
