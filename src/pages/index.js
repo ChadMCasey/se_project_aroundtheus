1a61ce123ab7c28cdcbc7a71a851499449e67956
Chad Casey <gbchadd@gmail.com>
2024-03-03 17:24:11 -0700

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Card from "../components/Card.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
-import Section from "../components/Section.js";
-import UserInfo from "../components/UserInfo.js";
 import "../pages/index.css";
 import { formValidationConfig, initialCards } from "../utils/constants.js";
 
